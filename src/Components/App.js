b5deea38325845921924866773fe89f374e3f0fd
Will Moretz <whmoretz@gmail.com>
2023-04-25 21:53:02 -0400

    added fetching of products from fake shop api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Contact from "./Contact";
+import "../index.css";
+
+async function fetchProducts() {
+  try {
+    const fetchedData = await fetch("https://fakestoreapi.com/products");
+    const data = await fetchedData.json();
+    const mensClothing = [];
+    const jewelry = [];
+    const electronics = [];
+    const womensClothing = [];
+    data.forEach((product) => {
+      if (product.category === "men's clothing") mensClothing.push(product);
+      if (product.category === "women's clothing") womensClothing.push(product);
+      if (product.category === "jewelery") jewelry.push(product);
+      if (product.category === "electronics") electronics.push(product);
+    });
+    return { mensClothing, womensClothing, jewelry, electronics };
+  } catch {
+    return "error";
+  }
+}
 
 function App() {
+  const [fetchError, setFetchError] = useState(false);
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    async function handleFetchedData() {
+      const fetchedProducts = await fetchProducts();
+      if (fetchedProducts === "error") setFetchError(true);
+      else setProducts(fetchedProducts);
+    }
+    handleFetchedData();
+  }, []);
+
   return (
     <>
       <Header />
