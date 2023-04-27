50352eeaedb2e15f3fa05a7a94a0713cb5b3198f
Will Moretz <whmoretz@gmail.com>
2023-04-26 23:31:04 -0400

    created product component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-function Product(props) {
+function Product() {
   const location = useLocation();
   const { productValues } = location.state;
-  return <section>{productValues.image}</section>;
+  return (
+    <section>
+      <h2>{productValues.title}</h2>
+      <div className="product-body">
+        <img src={productValues.image} alt={`${productValues.title}`} />
+        <p>{productValues.description}</p>
+      </div>
+      <p>{productValues.category}</p>
+      <p>{`Rating of ${productValues.rating.rate} with a total of ${productValues.rating.count} reviews`}</p>
+      <div className="checkout">
+        <div>{`$${productValues.price}`}</div>
+        <button type="button">Add to Cart</button>
+      </div>
+    </section>
+  );
 }
 
 export default Product;
