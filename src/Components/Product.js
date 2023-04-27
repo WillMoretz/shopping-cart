7083a862102a812fa37fcb457e5bb106297004b9
Will Moretz <whmoretz@gmail.com>
2023-04-27 10:40:43 -0400

    added manual url navigation to product handling

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 function Product() {
+  const [productValues, setProductValues] = useState(undefined);
+
+  const { id } = useParams();
+  if (id < 1 || id > 20 || !Number.isInteger(+id))
+    return <section>No Product Found</section>;
+
   const location = useLocation();
-  const { productValues } = location.state;
-  return (
+  if (location.state === null && productValues === undefined) {
+    (async () => {
+      const result = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const data = await result.json();
+      setProductValues(data);
+    })();
+  } else if (productValues === undefined) {
+    setProductValues(location.state.productValues);
+  }
+
+  return productValues === undefined ? (
+    <section>loading...</section>
+  ) : (
     <section>
       <h2>{productValues.title}</h2>
       <div className="product-body">
