78fad59df89a8f82f2a197fc1232024c5f6e77ab
Will Moretz <whmoretz@gmail.com>
2023-05-01 10:38:29 -0400

    created dropdown header nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,21 +5,30 @@ import logo from "../img/dune.svg";
 function Header() {
   return (
     <header>
-      <Link to="/" className="brand">
-        <img src={logo} alt="link to home page" className="logo" />
-        <h1>Sahara</h1>
-      </Link>
+      <div className="header-top">
+        <Link to="/" className="brand">
+          <img src={logo} alt="link to home page" className="logo" />
+          <h1>Sahara</h1>
+        </Link>
+        <button type="button" className="header-links-toggle">
+          Links
+        </button>
+      </div>
       <nav>
         <ul className="header-links">
+          <div className="line-break" aria-hidden="true" />
           <li>
             <Link to="/">Home</Link>
           </li>
+          <div className="line-break" aria-hidden="true" />
           <li>
             <Link to="/shop">Shop</Link>
           </li>
+          <div className="line-break" aria-hidden="true" />
           <li>
             <Link to="/contact">Contact</Link>
           </li>
+          <div className="line-break" aria-hidden="true" />
         </ul>
       </nav>
     </header>
