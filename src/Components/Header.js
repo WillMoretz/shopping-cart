d38004fb84d9c38eb5fdf1aa3a44e6472be614d3
Will Moretz <whmoretz@gmail.com>
2023-05-10 10:23:49 -0400

    made header nav automatically collapse after navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,39 +5,46 @@ import logo from "../img/dune.svg";
 function Header() {
   const [navIsHidden, setNavIsHidden] = useState(true);
 
-  function toggleHeaderLinks() {
-    if (navIsHidden) setNavIsHidden(false);
-    else setNavIsHidden(true);
+  function toggleHeaderLinks(allowedToOpen) {
+    if (navIsHidden) {
+      if (allowedToOpen) setNavIsHidden(false);
+    } else setNavIsHidden(true);
   }
 
   return (
     <header>
       <div className="header-top">
-        <Link to="/" className="brand">
+        <Link to="/" className="brand" onClick={() => toggleHeaderLinks(false)}>
           <img src={logo} alt="link to home page" className="logo" />
           <h1>Sahara</h1>
         </Link>
         <button
           type="button"
           className="header-links-toggle"
-          onClick={() => toggleHeaderLinks()}
+          onClick={() => toggleHeaderLinks(true)}
         >
-          Links
+          {navIsHidden ? "|||" : "X"}
         </button>
       </div>
       <nav>
         <ul className={`header-links ${navIsHidden ? "hidden" : "shown"}`}>
           <div className="line-break" aria-hidden="true" />
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => toggleHeaderLinks(true)}>
+              Home
+            </Link>
           </li>
           <div className="line-break" aria-hidden="true" />
           <li>
-            <Link to="/shop">Shop</Link>
+            <Link to="/shop" onClick={() => toggleHeaderLinks(true)}>
+              Shop
+            </Link>
           </li>
           <div className="line-break" aria-hidden="true" />
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" onClick={() => toggleHeaderLinks(true)}>
+              Contact
+            </Link>
           </li>
           <div className="line-break" aria-hidden="true" />
         </ul>
