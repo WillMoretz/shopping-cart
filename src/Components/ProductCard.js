061c7acb63a93b582bd2c6a3be4d51e8fcf06ed7
Will Moretz <whmoretz@gmail.com>
2023-05-11 22:30:24 -0400

    added truncate string function

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+function truncateString(str, desiredEnd) {
+  if (str.length < desiredEnd) return str;
+  let lowerEnd = desiredEnd;
+  let higherEnd = desiredEnd;
+  while (lowerEnd > -1 && higherEnd < str.length) {
+    if (str.charAt(lowerEnd) === " ") {
+      return `${str.slice(0, lowerEnd)}...`;
+    }
+    lowerEnd -= 1;
+    if (str.charAt(higherEnd) === " ") {
+      return `${str.slice(0, higherEnd)}...`;
+    }
+    higherEnd += 1;
+  }
+  return `${str.slice(0, desiredEnd)}...`;
+}
+
 function ProductCard(props) {
   const { product } = props;
   return (
@@ -9,8 +26,8 @@ function ProductCard(props) {
       className="product-card"
       state={{ productValues: product }}
     >
-      <h3>{product.title}</h3>
       <img src={product.image} alt={`${product.title}`} />
+      <h3>{truncateString(product.title, 40)}</h3>
       <div>{`$${product.price}`}</div>
     </Link>
   );
