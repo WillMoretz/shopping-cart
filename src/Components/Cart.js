2005a2385f2fde0e61a70548ef388f37ee1d34b4
Will Moretz <whmoretz@gmail.com>
2023-05-16 10:54:53 -0400

    added item display

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getCartItems } from "../cartItems";
 
 function Cart() {
-  return <div>Cart</div>;
+  const [cartItems, setCartItems] = useState(undefined);
+
+  useEffect(() => {
+    setCartItems(getCartItems());
+  }, []);
+
+  if (cartItems === undefined) return <section>loading...</section>;
+  if (cartItems.length === 0)
+    return (
+      <section>
+        <h2>Empty!</h2>
+      </section>
+    );
+  const itemsDisplayList = (
+    <ul>
+      {cartItems.map((item) => (
+        <Link
+          to={`../shop/${item.id}`}
+          className="cart-item"
+          key={`cart${item.title}${item.id}`}
+        >
+          <div>{item.title}</div>
+          <img src={item.image} alt={item.title} />
+          <div>{item.price}</div>
+        </Link>
+      ))}
+    </ul>
+  );
+  return <section>{itemsDisplayList}</section>;
 }
 
 export default Cart;
