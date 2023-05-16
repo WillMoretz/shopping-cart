81c98cfca0c8fde9971aab97b7bae8abe3125913
Will Moretz <whmoretz@gmail.com>
2023-05-15 21:03:24 -0400

    added cart route and link

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Header from "./Header";
 import Contact from "./Contact";
 import Home from "./Home";
 import Shop from "./Shop";
 import Product from "./Product";
+import Cart from "./Cart";
 import "../index.css";
 
 async function fetchProducts() {
@@ -50,7 +51,11 @@ function App() {
         />
         <Route path="/shop/:id" element={<Product />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Link to="/cart" className="cart-link">
+        Show Cart
+      </Link>
       <footer>
         Made by <i>Will Moretz</i>
       </footer>
