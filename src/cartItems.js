cb1c3bc6d6350fd32c9e73da39ab74d0b9488f58
Will Moretz <whmoretz@gmail.com>
2023-05-18 19:52:30 -0400

    added remove function

--- src/cartItems.js
+++ src/cartItems.js
@@ -2,7 +2,6 @@ const cart = [
   {
     image:
       "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    index: 0,
     id: 2,
     title: "Mens Casual Premium Slim Fit T-Shirts",
     price: 22.3,
@@ -10,7 +9,6 @@ const cart = [
 ];
 
 function addCartItem(item) {
-  Object.assign(item, { index: cart.length });
   cart.push(item);
 }
 
@@ -22,4 +20,8 @@ function getCartItems() {
   return cart;
 }
 
-export { addCartItem, clearCart, getCartItems };
+function removeCartItem(index) {
+  cart.splice(index, 1);
+}
+
+export { addCartItem, clearCart, getCartItems, removeCartItem };
