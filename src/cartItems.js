383e6076c8cd8ee3bda0f688fe0ab37ff9c1440e
Will Moretz <whmoretz@gmail.com>
2023-05-17 21:17:17 -0400

    TEMP: added placeholder values in cartItems

--- src/cartItems.js
+++ src/cartItems.js
@@ -1,4 +1,13 @@
-const cart = [];
+const cart = [
+  {
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    index: 0,
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts",
+    price: 22.3,
+  },
+];
 
 function addCartItem(item) {
   Object.assign(item, { index: cart.length });
