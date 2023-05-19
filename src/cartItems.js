7165b782e69355e7840a6f207aa818c1171c2e6e
Will Moretz <whmoretz@gmail.com>
2023-05-19 16:23:07 -0400

    removed placeholder cart item

--- src/cartItems.js
+++ src/cartItems.js
@@ -1,12 +1,4 @@
-const cart = [
-  {
-    image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    id: 2,
-    title: "Mens Casual Premium Slim Fit T-Shirts",
-    price: 22.3,
-  },
-];
+const cart = [];
 
 function addCartItem(item) {
   cart.push(item);
